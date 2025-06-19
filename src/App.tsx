c2d221890e61b64ea29d21446698ae1ef0115754
Anish Chaulagain <anishchaulagain2058@gmail.com>
2025-06-19 18:36:51 +0545

    Project Setup with React & TailwindCSS

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <div className='flex items-center justify-center h-screen'>
+    <h1 className='font-bold text-5xl'>Hello World</h1>
+    </div>
+      
+    </>
+  )
+}
+
+export default App
